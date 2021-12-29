b15a035361a96c9c1241348938959b129eb0ecff
nill <naadir.iqbal1@gmail.com>
2021-12-28 18:25:54 -0600

    all info added

--- src/contact-page.js
+++ src/contact-page.js
@@ -6,6 +6,16 @@ function contactPageLoad() {
     contactWindow.className ='window contact-window';
     contactWindow.textContent = 'CONTACT US';
 
+    let info = document.createElement('div');
+    info.className = 'contact-info';
+    info.textContent = 'If you would like to place an order please call us at: (555)555-5555';
+
+    let info2 = document.createElement('div');
+    info2.className = 'contact-info';
+    info2.textContent = 'If you have any other questions please call us at (555)555-5556';
+
+    contactWindow.appendChild(info);
+    contactWindow.appendChild(info2);
     flexContainer.appendChild(contactWindow);
 }
 
